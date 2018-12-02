549a5284d2f629e4dd76d1ac5d1cc6f9d698a0cc
11kim <fish.are.crafty@yandex.ru>
2018-12-02 10:05:31 +0300

    1

--- bhw/1.py
+++ bhw/1.py
@@ -24,30 +24,27 @@ def info():
 
 @app.route('/stats')
 def stats():
-    def stats():
-        file = open('data.txt', 'r', encoding='utf-8')
-        data = file.readlines()
-        file.close()
-        alts1 = {'ластик': set(), 'стирка': set(), 'резинка': set(), 'стёрка': set(), 'стиралка': set(),
+    file = open('data.txt', 'r', encoding='utf-8')
+    data = file.readlines()
+    file.close()
+    alts1 = {'ластик': set(), 'стирка': set(), 'резинка': set(), 'стёрка': set(), 'стиралка': set(),
                  'стирачка': set(), 'другое': set()}
-        alts2 = {'ластик': set(), 'стирка': set(), 'резинка': set(), 'стёрка': set(), 'стиралка': set(),
+    alts2 = {'ластик': set(), 'стирка': set(), 'резинка': set(), 'стёрка': set(), 'стиралка': set(),
                  'стирачка': set(), 'другое': set()}
-        for line in data:
-            if line == '':
-                break
-            line = line.rstrip()
-            line = line.split(';')
-            alts1[line[2]].add(line[0])
-            alts2[line[2]].add(line[1])
-        s = 'Распределение по городам\n'
-        for i in alts1:
-            s += i + ': ' + ', '.join(list(alts1[i])) + '\n'
-        s += 'Распределение по возрасту\n'
-        for i in alts2:
-            s += i + ': ' + ', '.join(list(alts2[i])) + '\n'
-        return s
-
-    stats()
+    for line in data:
+        if line == '':
+            break
+        line = line.rstrip()
+        line = line.split(';')
+        alts1[line[2]].add(line[0])
+        alts2[line[2]].add(line[1])
+    s = 'Распределение по городам<br>'
+    for i in alts1:
+        s += i + ': ' + ', '.join(list(alts1[i])) + '<br>'
+    s += 'Распределение по возрасту<br>'
+    for i in alts2:
+        s += i + ': ' + ', '.join(list(alts2[i])) + '<br>'
+    return s
 
 
 app.run(debug='True')
